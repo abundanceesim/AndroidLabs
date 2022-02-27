08d524a649073db6b70c37d0fead936c6d197fa2
Abundance Esim <esim0001@algonquinlive.com>
2022-02-27 17:38:29 -0500

    Finished lab 4.

--- app/src/main/java/com/cst2335/esim0001/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/esim0001/ChatRoomActivity.java
@@ -1,5 +1,6 @@
 package com.cst2335.esim0001;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,9 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,14 +20,11 @@ import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
     public static final String TAG = "ChatRoomActivity";
-    public static String messageType;
     EditText message;
     Button sendButton;
     Button receiveButton;
-    boolean sendClicked;
-    boolean receiveClicked;
-    MyListAdapter myAdapter;   //<<cannot be anonymous<<
-    ArrayList<Message> messages = new ArrayList<Message>(); // was formerly of type Message, could be changed back soon.
+    ChatAdapter myAdapter;   //<<cannot be anonymous<<
+    ArrayList<Message> messages = new ArrayList<Message>();  //array list that would contain messages.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +32,77 @@ public class ChatRoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat_room);
 
         ListView myList = findViewById(R.id.listView);
-        myList.setAdapter( myAdapter = new MyListAdapter());
+        myList.setAdapter( myAdapter = new ChatAdapter());
 
-        sendClicked = false;
-        receiveClicked = false;
 
-        //if send button is clicked, message is being sent, use the layout for sending a message
         sendButton = findViewById(R.id.sendButton);
         receiveButton = findViewById(R.id.receiveButton);
         message = findViewById(R.id.messageText);
 
-
+        //if send button is clicked use the layout for sending a message
         sendButton.setOnClickListener( (click) -> {
             String typedMessage = message.getText().toString();
             Log.e(TAG, "Send button clicked");
+
+            //Message thisRow = messages.get(position);
             if ( !typedMessage.isEmpty()) {
-                messages.add(new Message(typedMessage));
+                messages.add(new Message(typedMessage, "send"));
 
-                message.setText(typedMessage);//clear the text
+                message.setText(""); //clear any text from the EditText.
 
-                //notify that new data was added at a row:
-                myAdapter.notifyDataSetChanged(); //at the end of ArrayList,
+                //notify adapter that new data was added at a row:
+                myAdapter.notifyDataSetChanged();
 
             }
         });
 
+        //if receive button is clicked use the layout for receiving a message
         receiveButton.setOnClickListener( (click) -> {
             Log.e(TAG, "Receive button clicked");
-            receiveClicked = true;
+            String typedMessage = message.getText().toString();
+
+            //Message thisRow = messages.get(position);
+            if ( !typedMessage.isEmpty()) {
+                messages.add(new Message(typedMessage, "receive"));
+
+                message.setText("");   //clear any text from the EditText.
+
+                //notify adapter that new data was added at a row:
+                myAdapter.notifyDataSetChanged();
+
+            }
         });
+
+        /*Listener for listview entries.*/
+        myList.setOnItemLongClickListener( (p, b, pos, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Do you want to delete this?")
+
+                    .setMessage("The selected row is: " + pos + "\nThe database id is: " + id)
+
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        messages.remove(pos);
+                        myAdapter.notifyDataSetChanged();
+                    })
+
+                    .setNegativeButton("No", (click, arg) -> { })
+
+                    //Display the Alert Dialog.
+                    .create().show();
+            return true;
+        });
+
 
     }
 
-    private class MyListAdapter extends BaseAdapter {
+    private class ChatAdapter extends BaseAdapter {
 
         public int getCount() { return messages.size();}
 
-        public Object getItem(int position) { return message.getText().toString(); }
+        public Object getItem(int position) {
+            Message thisRow = messages.get(position);
+            return thisRow.messageTyped;
+        }
 
         public long getItemId(int position) { return (long) position; }
 
@@ -76,31 +110,45 @@ public class ChatRoomActivity extends AppCompatActivity {
         {
             LayoutInflater inflater = getLayoutInflater();
 
-            //make a new row:
+            //New row for the Send Layout
             View sendView = inflater.inflate(R.layout.sent_message, parent, false);
-
-            //set what the text should be for this row:
             TextView sentMessage = sendView.findViewById(R.id.sendText);
-            sentMessage.setText( getItem(position).toString() );
+            sentMessage.setText( getItem(position).toString() );         //set text to be displayed for this row.
+            ImageView sendImage = sendView.findViewById(R.id.sendImage); //image to be displayed for sent messages
 
-            //Button b =  newView.findViewById(R.id.textGoesHere);
-            //b.setText( getItem(position).toString() );
+            //New row for the Receive Layout
+            View receiveView = inflater.inflate(R.layout.received_message, parent, false);
+            TextView receivedMessage = receiveView.findViewById(R.id.receiveText);
+            receivedMessage.setText( getItem(position).toString() );              //set text to be displayed for this row.
+            ImageView receiveImage = receiveView.findViewById(R.id.receiveImage); //image to be displayed for received messages
 
-            //return it to be put in the table
-            return sentMessage;
+
+            Message thisRow = messages.get(position);
+            //if the value for the type of message is "send", return the send view, otherwise return the receive view.
+            if (thisRow.sendOrReceive.equals("send"))
+                return sendView;
+
+
+            return receiveView;
         }
     }
 
-
+    /*Message class would be used for creating new messages and adding them to the Array List.*/
     public class Message{
         String messageTyped;
+        String sendOrReceive; //String to show whether message is being sent, or received.
 
-        public Message(String messageTyped) {
+        public Message(String messageTyped, String sendOrReceive) {
             this.messageTyped = messageTyped;
+            this.sendOrReceive = sendOrReceive;
         }
 
         public String getMessageTyped() {
             return messageTyped;
+        }
+
+        public String getSendOrReceive() {
+            return sendOrReceive;
         }
     }
 
